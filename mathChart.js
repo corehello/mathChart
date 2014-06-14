37bd68797768f2dd1c2bfed6d2a4ff701655cdd5
corehello <corehello@gmail.com>
2014-06-14 13:37:29 +0800

    init the function to insert chart

--- mathChart.js
+++ mathChart.js
@@ -1,2 +1,20 @@
 var mathChart = {
+	var insertchart = function(canvas,data)
+	{
+		var xmlns="https://www.w3.org/2000/svg"
+		var svg = document.createElementNS(xmlns,"svg");
+		svg.height = canvas.clientHeight;
+		svg.width = canvas.clientWidth;
+		var path = generatePath(svg,data);
+		for(var i=0; i<path.length; ++i)
+		{
+			svg.appendChild(path[i]);
+		}
+		canvas.appendChild(svg);
+	};
+	
+	var generatePath(svg, data)
+	{
+		
+	}
 }
