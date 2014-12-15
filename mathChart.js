e03aa8d8c9c4e1ca8105e520b9370dcb59c5afb3
corehello <corehello@gmail.com>
2014-12-15 21:57:59 +0800

    debug with mathChart.js

--- mathChart.js
+++ mathChart.js
@@ -1,6 +1,6 @@
 /*
 	mathChart.js 
-	author: corehello(corehello@gmail.com)
+	author: corehello <corehello@gmail.com>
 	argument:
 	data = 
 	{
@@ -15,33 +15,33 @@
 
 
 var mathChart = {
-	var xmlns="https://www.w3.org/2000/svg";
-	var insertchart = function(canvas,data)
+	xmlns : "https://www.w3.org/2000/svg",
+	insertChart : function(canvas,data)
 	{
-		var svg = document.createElementNS(xmlns,"svg");
+		var svg = document.createElementNS(this.xmlns,"svg");
 		svg.height = canvas.clientHeight;
 		svg.width = canvas.clientWidth;
-		var path = generatePath(svg,data);
+		var path = this.generatePath(svg,data);
 		for(var i=0; i<path.length; ++i)
 		{
 			svg.appendChild(path[i]);
 		}
 		canvas.appendChild(svg);
-	};
+	},
 	
-	var generatePath = function(svg, data)
+	generatePath : function(svg, data)
 	{
 		var paths=[];
 		for(i=0; i<data.data.length; ++i)
 		{
 			if(data.data[i][0].length == 2)
 			{
-				var dataTrans = transferData2D(svg, data.range, data.data[i]);
+				var dataTrans = this.transferData2D(svg, data.range, data.data[i]);
 				var dd = dataTrans.join(" ");
 			}
 			else
 			{
-				var dataTrans = transferData3D(svg, data.range, data.data[i]);
+				var dataTrans = this.transferData3D(svg, data.range, data.data[i]);
 				var dd = dataTrans.join(" ");
 				
 			}
@@ -52,9 +52,9 @@ var mathChart = {
 			paths.push(path);
 		}
 		return paths;
-	};
+	},
 	
-    var adaptRange = function(x,y,range, data)
+    adaptRange : function(x,y,range, data)
     {
         if (range.length == 0)
         {
@@ -70,23 +70,24 @@ var mathChart = {
             }
             r_x.sort(function(a,b){return a-b});
             r_y.sort(function(a,b){return a-b});
-            rr =  [[r_x[0],r_x[r_x.length]],[r_y[0],r_y[r_y.length*3/2]]]；
+            var rr =  [[r_x[0],r_x[r_x.length]],[r_y[0],r_y[r_y.length*3/2]]];
         }
         else
         {
-            rr =  range;
+            var rr =  range;
         }
+        return rr;
 
-    }
+    },
 
-	var transferData2D = function(svg, range, data)
+	transferData2D : function(svg, range, data)
 	{
-	    range_r = adaptRange(svg.width, svg.height, range, data);
+	    range_r = this.adaptRange(svg.width, svg.height, range, data);
         
-	}
+	},
 	
-	var transferData3D = function(svg, range, data)
+	transferData3D : function(svg, range, data)
 	{
-	    range_r = adaptRange(svg.width, svg.height, range, data);
+	    range_r = this.adaptRange(svg.width, svg.height, range, data);
 	}
 }
